840e17f27a04bdd40a7a083339f261ab2b6d3590
Roman Pirshtuk <pirshtukroman@gmail.com>
2024-07-09 20:47:42 +0300

    Initial Commit

---
+++ WearHouseApp/WearHouseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WearHouseAppApp.swift
+//  WearHouseApp
+//
+//  Created by Roman on 09/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WearHouseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
